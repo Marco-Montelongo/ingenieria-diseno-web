cbfccec5c6ed998cbef0e1db4dd30faf980e2c0c
Marco-Montelongo <marco.montelongo@ingenieria.unam.edu>
2026-01-30 22:43:58 -0600

    Update generateCoverPdf.js

--- js/pdf/generateCoverPdf.js
+++ js/pdf/generateCoverPdf.js
@@ -40,8 +40,8 @@ export async function generateCoverPdf(formDef) {
     /* =====================
        ESCUDOS (SIN APLASTAR)
     ====================== */
-    const escudoFI = await loadImage("../../img/escudoFI.png");
-    const escudoUNAM = await loadImage("../../img/escudoUNAM.png");
+    const escudoFI = await loadImage("img/escudoFI.png");
+    const escudoUNAM = await loadImage("img/escudoUNAM.png");
 
     const escudoW = 22;
     const fi = pdf.getImageProperties(escudoFI);
@@ -158,4 +158,5 @@ export async function generateCoverPdf(formDef) {
     ====================== */
     pdf.save(`CoverPage_Equipo_${data.teamNumber || "NA"}.pdf`);
 }
+
 
